3644102e7ff9bc3adbb377cfbe97601bb89a38d9
Camila Santa Maria Luna <camilorris97@gmail.com>
2019-10-14 19:33:43 -0500

    This commit is alive

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import Navbar from "./components/Navbar";
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <h1>Hello from App</h1>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import styles from "./styles.module.css";
+
+const Navbar = () => {
+  return <h1 className={styles.navbarTitle}>Navbar</h1>;
+};
+
+export default Navbar;
