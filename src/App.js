1b8a65bb4e20fdf1fbdd4858aa2da06a8127e509
Camila Santa Maria Luna <camilorris97@gmail.com>
2019-10-17 13:08:11 -0500

    Create novias section

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import Novias from "./components/Novias";
 
 const App = () => {
   return (
     <main>
       <Navbar />
       <Hero />
+      <Novias />
     </main>
   );
 };
