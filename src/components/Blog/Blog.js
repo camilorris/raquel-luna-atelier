accea2bb2ded305262c346faddc95b050d404788
Camila Luna <45946769+camilorris@users.noreply.github.com>
2019-10-25 13:01:32 -0500

    Add blog section (#5)

---
+++ src/components/Blog/Blog.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+import styles from "./styles.module.scss";
+import Lace from "assets/images/lace.png";
+
+const Blog = () => {
+  return (
+    <div className={styles.blogContainer}>
+      <img src={Lace} alt="encaje" className={styles.lacePic} />
+      <div className={styles.text}>
+        <h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h4>
+        <p>
+          Integer molestie maximus magna, et ultricies sapien lobortis eu. Fusce
+          vulputate ullamcorper metus, eget feugiat magna lacinia pellentesque.
+          Nulla faucibus consectetur auctor. Mauris eget eros tincidunt,
+          vulputate...
+        </p>
+        <a href="google.com">LEER MÁS</a>
+      </div>
+    </div>
+  );
+};
+
+export default Blog;
